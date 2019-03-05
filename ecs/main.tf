9c02082ef9dbd95e8a0bb22f36eb2cf267afb3cf
toshi-miura <toshihiko.miura@gmail.com>
2019-03-05 07:46:08 +0000

    circleCIで、terraform実行用のテンプレを追加（最終的には、ecs taskの作成を入れる）

---
+++ ecs/main.tf
@@ -0,0 +1,27 @@
+# tfstateの設定
+terraform {
+  backend "s3" {
+    encrypt = "true"
+    region = "ap-northeast-1"
+    bucket = "${var.tf_s3_backend}"
+    key = "ecs/terraform.tfstate"
+  }
+}
+
+
+
+# AWS Providerの設定
+provider "aws" {
+  region = "ap-northeast-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+
+# IAMユーザの設定
+resource "aws_iam_user" "iam_users" {
+  count = "${length(var.iam_users)}"
+  name = "${element(var.iam_users, count.index)}"
+  path = "/"
+  force_destroy = false
+}
---
+++ ecs/variables.tf
@@ -0,0 +1,21 @@
+
+### circleCIのENVより読み込み
+
+###  TF_VAR_access_key 
+###  TF_VAR_secret_key
+###  TF_VAR_tf_s3_backend
+
+variable "access_key" {}
+variable "secret_key" {}
+variable "tf_s3_backend" {}
+
+
+# IAMユーザ一覧 TEST用
+variable "iam_users" {
+  type = "list"
+  default = [
+    "iam_user1",
+    "iam_user2",
+    "iam_user3",
+  ]
+}
