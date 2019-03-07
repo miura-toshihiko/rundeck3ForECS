125aa248e516b468ed3d5b98fc1d9e1a3c2fcd08
toshi-miura <toshihiko.miura@gmail.com>
2019-03-07 10:42:05 +0000

    no message

--- ecs/paramater_store.tf
+++ ecs/paramater_store.tf
@@ -25,4 +25,52 @@ resource "aws_ssm_parameter" "RUNDECK_PLUGIN_EXECUTIONFILESTORAGE_S3_BUCKET" {
   type  = "SecureString"
   value = "${var.RUNDECK_PLUGIN_EXECUTIONFILESTORAGE_S3_BUCKET}"
 }
+resource "aws_ssm_parameter" "OAUTH2_PROXY_GITHUB_ORG" {
+  name  = "OAUTH2_PROXY_GITHUB_ORG"
+  type  = "SecureString"
+  value = "${var.OAUTH2_PROXY_GITHUB_ORG}"
+
+}
+resource "aws_ssm_parameter" "OAUTH2_PROXY_GITHUB_TEAM" {
+  name  = "OAUTH2_PROXY_GITHUB_TEAM"
+  type  = "SecureString"
+  value = "${var.OAUTH2_PROXY_GITHUB_TEAM}"
+  
+}
+resource "aws_ssm_parameter" "OAUTH2_PROXY_REDIREC_URL" {
+  name  = "OAUTH2_PROXY_REDIREC_URL"
+  type  = "SecureString"
+  value = "${var.OAUTH2_PROXY_REDIREC_URL}"
+  
+}
+
+resource "aws_ssm_parameter" "OAUTH2_PROXY_COOKIE_DOMAIN" {
+  name  = "OAUTH2_PROXY_COOKIE_DOMAIN"
+  type  = "SecureString"
+  value = "${var.OAUTH2_PROXY_COOKIE_DOMAIN}"
+  
+}
+
+
+resource "aws_ssm_parameter" "OAUTH2_PROXY_CLIENT_ID" {
+  name  = "OAUTH2_PROXY_CLIENT_ID"
+  type  = "SecureString"
+  value = "${var.OAUTH2_PROXY_CLIENT_ID}"
+  
+}
+
+resource "aws_ssm_parameter" "OAUTH2_PROXY_CLIENT_SECRET" {
+  name  = "OAUTH2_PROXY_CLIENT_SECRET"
+  type  = "SecureString"
+  value = "${var.OAUTH2_PROXY_CLIENT_SECRET}"
+  
+}
+
+resource "aws_ssm_parameter" "OAUTH2_PROXY_COOKIE_SECRET" {
+  name  = "OAUTH2_PROXY_COOKIE_SECRET"
+  type  = "SecureString"
+  value = "${var.OAUTH2_PROXY_COOKIE_SECRET}"
+  
+}
+
 
