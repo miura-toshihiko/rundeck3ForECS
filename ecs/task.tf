7118d0a04edcd822aeddb8bcc7661f0028456a2a
miura-toshihiko <miura.toshihiko@isid.co.jp>
2019-04-10 16:52:24 +0900

    Update task.tf

--- ecs/task.tf
+++ ecs/task.tf
@@ -4,4 +4,8 @@ resource "aws_ecs_task_definition" "service" {
 
   task_role_arn  = "arn:aws:iam::755575973821:role/ecsTaskExecutionRole"
   execution_role_arn  = "arn:aws:iam::755575973821:role/ecsTaskExecutionRole"
+  network_mode ="awsvpc"
+  cpu =1024
+  memory =2048
+    
 }
