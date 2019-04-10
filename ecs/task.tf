0f9503ff393d4919eec2e01d0ce1c64f2fe97616
miura-toshihiko <miura.toshihiko@isid.co.jp>
2019-04-10 16:02:31 +0900

    Update task.tf

--- ecs/task.tf
+++ ecs/task.tf
@@ -2,5 +2,6 @@ resource "aws_ecs_task_definition" "service" {
   family                = "rundeck3ForECS"
   container_definitions = "${file("task-definitions/service.json")}"
 
-
+  task_role_arn  = "arn:aws:iam::755575973821:role/ecsTaskExecutionRole"
+  execution_role_arn  = "arn:aws:iam::755575973821:role/ecsTaskExecutionRole"
 }
