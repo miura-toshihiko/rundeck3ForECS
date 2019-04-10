1f64d6368eef38c17fa21b062ecdcbdadf5028dc
miura-toshihiko <miura.toshihiko@isid.co.jp>
2019-04-10 14:18:12 +0900

    Update task.tf

--- ecs/task.tf
+++ ecs/task.tf
@@ -1,6 +1,6 @@
 resource "aws_ecs_task_definition" "service" {
   family                = "rundeck3ForECS"
-  container_definitions = "${file("./tfwork/ecs/task-definitions/service.json")}"
+  container_definitions = "${file("task-definitions/service.json")}"
 
 
 }
